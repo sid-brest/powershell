8920a57229f19f04f4aab3fef4fe1e794bd8d761
Dzmitry Struneuski <siiid@mail.ru>
2022-02-05 00:52:56 +0300

    ps_16_1_v01

--- PowerShell_Essentials_16/PowerShell Essentials 16-1.ps1
+++ PowerShell_Essentials_16/PowerShell Essentials 16-1.ps1
@@ -1,2 +1,13 @@
 # Write an advanced function to show the size of files 
 # in specific folder and sort all of them by size.
+# Get-FilesSize -Path "c:\Windows"
+function Get-FilesSize {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]$Path
+    )
+    Get-ChildItem -Path $Path -File |      
+    Select-Object -Property Name, @{label = "Size (MB)"; expression = { $_.Length / 1MB } } |
+    Sort-Object -Property "Size" | Format-Table  
+} 

3c401265c67fb95a89d64f55422ddae5ee8221d4
Dzmitry Struneuski <siiid@mail.ru>
2022-02-05 00:08:51 +0300

    ps_16_v0

---
+++ PowerShell_Essentials_16/PowerShell Essentials 16-1.ps1
@@ -0,0 +1,2 @@
+# Write an advanced function to show the size of files 
+# in specific folder and sort all of them by size.
---
+++ PowerShell_Essentials_16/PowerShell Essentials 16-2.ps1
@@ -0,0 +1,2 @@
+# Write an advanced function to delete file from a specific folder 
+# if file size greater then specified size in Kb.

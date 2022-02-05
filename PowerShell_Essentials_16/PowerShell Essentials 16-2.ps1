c070ed6c5682e1950f1902cbee4f280fd1e82abc
Dzmitry Struneuski <siiid@mail.ru>
2022-02-05 23:03:42 +0300

    ps_16_2_final

--- PowerShell_Essentials_16/PowerShell Essentials 16-2.ps1
+++ PowerShell_Essentials_16/PowerShell Essentials 16-2.ps1
@@ -1,2 +1,12 @@
 # Write an advanced function to delete file from a specific folder 
-# if file size greater then specified size in Kb.
+# if file size greater then specified size in Kb
+function DelFileSize {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]$Folder,
+        [Int]$Size
+    )
+    Get-ChildItem -Path $Folder -File | Where-Object { ($_.Length / 1KB) -gt $Size } | 
+    Remove-Item -Verbose
+}
